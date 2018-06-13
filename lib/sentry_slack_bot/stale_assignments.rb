2199676d41010060d1600c3bb5370cebe7d7420e
Jon Bauer <bauerjon@hotmail.com>
2018-06-13 09:43:19 -0500

    utilize intance variable without leaving it around for ever

--- lib/sentry_slack_bot/stale_assignments.rb
+++ lib/sentry_slack_bot/stale_assignments.rb
@@ -1,76 +1,78 @@
 module SentrySlackBot
   class StaleAssignments
-    class << self
-      def notify!
-        stale_issues_by_assignee = Hash.new{|h,k| h[k]=[]}
+    def self.notify!
+      self.new.notify!
+    end
 
-        assigned_unresolved_sentry_issues.each do |issue|
-          issue_details = issue_details_from_id(issue['id'])
+    def notify!
+      stale_issues_by_assignee = Hash.new{|h,k| h[k]=[]}
 
-          if stale_assignment?(issue_details) && !recent_comments?(issue_details)
-            stale_issues_by_assignee[issue_details['assignedTo']['email']] << issue_details['permalink']
-          end
-        end
+      assigned_unresolved_sentry_issues.each do |issue|
+        issue_details = issue_details_from_id(issue['id'])
 
-        message_text = "The following unresolved issues have been assigned to you for over 1 week without resolve or additional comments. Please comment on issue with any updates/fix the issue/'ingore until X'/resolve/unassign. Otherwise you will continue to be notified during business hours.\n\n"
-
-        stale_issues_by_assignee.each do |email, issue_links|
-          name = get_slack_name_from_email(email) || email
-          message_text += "#{name}:\n\n#{issue_links.join("\n")}\n\n"
-        end
-
-        send_message_to_channel(SentrySlackBot::Config.slack_channel_id, message_text)
-      end
-
-      private
-
-      def assigned_unresolved_sentry_issues
-        @assigned_unresolved_sentry_issues ||= (
-          headers = { "Authorization" => "Bearer #{SentrySlackBot::Config.sentry_api_token}" }
-          sentry_projects.flat_map do |project|
-            response = HTTParty.get("https://app.getsentry.com/api/0/projects/#{SentrySlackBot::Config.sentry_organization_slug}/#{project['slug']}/issues/?query=is:unresolved is:assigned", headers: headers)
-            JSON.parse(response.body)
-          end
-        )
-      end
-
-      def stale_assignment?(issue)
-        date = issue['activity'].find{|a| a['type'] == 'assigned'}['dateCreated']
-        DateTime.parse(date) < (Time.now - SentrySlackBot::Config.stale_assignment_grace_period)
-      end
-
-      def recent_comments?(issue)
-        note = issue['activity'].find{|a| a['type'] == 'note'}
-
-        if note && note['dateCreated']
-          DateTime.parse(note['dateCreated']) > (Time.now - SentrySlackBot::Config.stale_assignment_grace_period)
-        else
-          false
+        if stale_assignment?(issue_details) && !recent_comments?(issue_details)
+          stale_issues_by_assignee[issue_details['assignedTo']['email']] << issue_details['permalink']
         end
       end
 
-      def issue_details_from_id(issue_id)
+      message_text = "The following unresolved issues have been assigned to you for over 1 week without resolve or additional comments. Please comment on issue with any updates/fix the issue/'ingore until X'/resolve/unassign. Otherwise you will continue to be notified during business hours.\n\n"
+
+      stale_issues_by_assignee.each do |email, issue_links|
+        name = get_slack_name_from_email(email) || email
+        message_text += "#{name}:\n\n#{issue_links.join("\n")}\n\n"
+      end
+
+      send_message_to_channel(SentrySlackBot::Config.slack_channel_id, message_text)
+    end
+
+    private
+
+    def assigned_unresolved_sentry_issues
+      @assigned_unresolved_sentry_issues ||= (
         headers = { "Authorization" => "Bearer #{SentrySlackBot::Config.sentry_api_token}" }
-        response = HTTParty.get("https://app.getsentry.com/api/0/issues/#{issue_id}/", headers: headers)
-        JSON.parse(response.body)
-      end
+        sentry_projects.flat_map do |project|
+          response = HTTParty.get("https://app.getsentry.com/api/0/projects/#{SentrySlackBot::Config.sentry_organization_slug}/#{project['slug']}/issues/?query=is:unresolved is:assigned", headers: headers)
+          JSON.parse(response.body)
+        end
+      )
+    end
 
-      def sentry_projects
-        headers = { "Authorization" => "Bearer #{SentrySlackBot::Config.sentry_api_token}" }
-        response = HTTParty.get("https://app.getsentry.com//api/0/organizations/#{SentrySlackBot::Config.sentry_organization_slug}/projects/", headers: headers)
-        JSON.parse(response.body)
-      end
+    def stale_assignment?(issue)
+      date = issue['activity'].find{|a| a['type'] == 'assigned'}['dateCreated']
+      DateTime.parse(date) < (Time.now - SentrySlackBot::Config.stale_assignment_grace_period)
+    end
 
-      def get_slack_name_from_email(email)
-        response = HTTParty.get("https://slack.com/api/users.lookupByEmail?email=#{email}&token=#{SentrySlackBot::Config.slack_api_token}")
-        slack_name = JSON.parse(response.body).dig('user','name')
-        slack_name ? "@#{slack_name}" : nil
-      end
+    def recent_comments?(issue)
+      note = issue['activity'].find{|a| a['type'] == 'note'}
 
-      def send_message_to_channel(channel, text)
-        response = HTTParty.post("https://slack.com/api/chat.postMessage?token=#{SentrySlackBot::Config.slack_api_token}&channel=#{channel}&text=#{URI.encode(text)}&as_user=false&link_names=1")
-        JSON.parse(response.body)
+      if note && note['dateCreated']
+        DateTime.parse(note['dateCreated']) > (Time.now - SentrySlackBot::Config.stale_assignment_grace_period)
+      else
+        false
       end
+    end
+
+    def issue_details_from_id(issue_id)
+      headers = { "Authorization" => "Bearer #{SentrySlackBot::Config.sentry_api_token}" }
+      response = HTTParty.get("https://app.getsentry.com/api/0/issues/#{issue_id}/", headers: headers)
+      JSON.parse(response.body)
+    end
+
+    def sentry_projects
+      headers = { "Authorization" => "Bearer #{SentrySlackBot::Config.sentry_api_token}" }
+      response = HTTParty.get("https://app.getsentry.com//api/0/organizations/#{SentrySlackBot::Config.sentry_organization_slug}/projects/", headers: headers)
+      JSON.parse(response.body)
+    end
+
+    def get_slack_name_from_email(email)
+      response = HTTParty.get("https://slack.com/api/users.lookupByEmail?email=#{email}&token=#{SentrySlackBot::Config.slack_api_token}")
+      slack_name = JSON.parse(response.body).dig('user','name')
+      slack_name ? "@#{slack_name}" : nil
+    end
+
+    def send_message_to_channel(channel, text)
+      response = HTTParty.post("https://slack.com/api/chat.postMessage?token=#{SentrySlackBot::Config.slack_api_token}&channel=#{channel}&text=#{URI.encode(text)}&as_user=false&link_names=1")
+      JSON.parse(response.body)
     end
   end
 end
--- lib/sentry_slack_bot/unattended_issues.rb
+++ lib/sentry_slack_bot/unattended_issues.rb
@@ -1,65 +1,70 @@
 module SentrySlackBot
   class UnattendedIssues
-    class << self
-      def notify!
-        slack_channels_messages.each do |message|
-          title_link = message.try(:[], 'attachments').try(:first).try(:[], 'title_link')
+    def self.notify!
+      self.new.notify!
+    end
 
-          if title_link
-            match_data = /.*issues\/(?<issue_id>.*)\/.*/.match(title_link)
-            next unless match_data
-            issue = issue_from_id(match_data['issue_id'])
+    def notify!
+      slack_channels_messages.each do |message|
+        title_link = message.try(:[], 'attachments').try(:first).try(:[], 'title_link')
 
-            if issue
-              ts = message['ts']
-              group_to_notify = SentrySlackBot::Config.slack_group_per_sentry_project[issue['project']['slug']]
-              text = "Hey there #{group_to_notify}! This bug is unresolved and has not been assigned in Sentry. *Please assign/resolve/or 'Ignore until happens X times...'*\n If left unassigned/unresolved/unignored you will continue to receive notifications during business hours."
-              send_message_to_thread(ts, text)
-            end
+        if title_link
+          match_data = /.*issues\/(?<issue_id>.*)\/.*/.match(title_link)
+          next unless match_data
+          issue = issue_from_id(match_data['issue_id'])
+
+          if issue
+            ts = message['ts']
+            group_to_notify = SentrySlackBot::Config.slack_group_per_sentry_project[issue['project']['slug']]
+            text = "Hey there #{group_to_notify}! This bug is unresolved and has not been assigned in Sentry. *Please assign/resolve/or 'Ignore until happens X times...'*\n If left unassigned/unresolved/unignored you will continue to receive notifications during business hours."
+
+            send_message_to_thread(ts, text)
           end
         end
       end
+    end
 
-      def issue_from_id(issue_id)
-        unassigned_unresolved_sentry_issues.find{|a| a['id'] == issue_id}
-      end
+    private
 
-      def unassigned_unresolved_sentry_issues
-        @unassigned_unresolved_sentry_issues ||= (
-          headers = { "Authorization" => "Bearer #{SentrySlackBot::Config.sentry_api_token}" }
-          sentry_projects.flat_map do |project|
-            response = HTTParty.get("https://app.getsentry.com/api/0/projects/#{SentrySlackBot::Config.sentry_organization_slug}/#{project['slug']}/issues/?statsPeriod=14d&query=is:unresolved is:unassigned", headers: headers)
-            JSON.parse(response.body)
-          end
-        )
-      end
+    def issue_from_id(issue_id)
+      unassigned_unresolved_sentry_issues.find{|a| a['id'] == issue_id}
+    end
 
-      def sentry_projects
+    def unassigned_unresolved_sentry_issues
+      @unassigned_unresolved_sentry_issues ||= (
         headers = { "Authorization" => "Bearer #{SentrySlackBot::Config.sentry_api_token}" }
-        response = HTTParty.get("https://app.getsentry.com//api/0/organizations/#{SentrySlackBot::Config.sentry_organization_slug}/projects/", headers: headers)
-        JSON.parse(response.body)
-      end
-
-      def slack_channels_messages
-        messages = []
-        has_more = true
-        oldest = Time.parse(SentrySlackBot::Config.unattended_issue_message_cut_off_date).to_i
-
-        while has_more do
-          response = HTTParty.get("https://slack.com/api/channels.history?token=#{SentrySlackBot::Config.slack_api_token}&channel=#{SentrySlackBot::Config.slack_channel_id}&oldest=#{oldest}")
-          json_body = JSON.parse(response.body)
-          messages << json_body['messages'].reverse
-          oldest = json_body['messages'].first['ts']
-          has_more = json_body['has_more']
+        sentry_projects.flat_map do |project|
+          response = HTTParty.get("https://app.getsentry.com/api/0/projects/#{SentrySlackBot::Config.sentry_organization_slug}/#{project['slug']}/issues/?statsPeriod=14d&query=is:unresolved is:unassigned", headers: headers)
+          JSON.parse(response.body)
         end
+      )
+    end
 
-        messages.flatten
+    def sentry_projects
+      headers = { "Authorization" => "Bearer #{SentrySlackBot::Config.sentry_api_token}" }
+      response = HTTParty.get("https://app.getsentry.com//api/0/organizations/#{SentrySlackBot::Config.sentry_organization_slug}/projects/", headers: headers)
+      JSON.parse(response.body)
+    end
+
+    def slack_channels_messages
+      messages = []
+      has_more = true
+      oldest = Time.parse(SentrySlackBot::Config.unattended_issue_message_cut_off_date).to_i
+
+      while has_more do
+        response = HTTParty.get("https://slack.com/api/channels.history?token=#{SentrySlackBot::Config.slack_api_token}&channel=#{SentrySlackBot::Config.slack_channel_id}&oldest=#{oldest}")
+        json_body = JSON.parse(response.body)
+        messages << json_body['messages'].reverse
+        oldest = json_body['messages'].first['ts']
+        has_more = json_body['has_more']
       end
 
-      def send_message_to_thread(ts, text)
-        response = HTTParty.post("https://slack.com/api/chat.postMessage?token=#{SentrySlackBot::Config.slack_api_token}&channel=#{SentrySlackBot::Config.slack_channel_id}&text=#{URI.encode(text)}&reply_broadcast=false&thread_ts=#{ts}&as_user=false&link_names=1")
-        JSON.parse(response.body)
-      end
+      messages.flatten
+    end
+
+    def send_message_to_thread(ts, text)
+      response = HTTParty.post("https://slack.com/api/chat.postMessage?token=#{SentrySlackBot::Config.slack_api_token}&channel=#{SentrySlackBot::Config.slack_channel_id}&text=#{URI.encode(text)}&reply_broadcast=false&thread_ts=#{ts}&as_user=false&link_names=1")
+      JSON.parse(response.body)
     end
   end
 end
